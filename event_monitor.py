e0262a77bcb119ee7d4662804c5d548856563c4e
tawastory <tawa2608@gmail.com>
2024-04-09 13:16:06 +0900

    Update event_monitor.py

--- event_monitor.py
+++ event_monitor.py
@@ -4,6 +4,8 @@ import kubernetes.config
 import traceback
 import sys
 import datetime
+from datetime import timedelta
+from pytz import timezone
 
 def get_client(**kwargs):
     try:
@@ -20,6 +22,13 @@ token_auth = dict(
     verify_ssl=False
 )
 
+def check_monitoring_time(event_time):
+    previous_date = datetime.datetime.utcnow() - timedelta(minutes=5)
+
+    return event_time > previous_date
+
+nodeEventList = ['NodeNotReady','FailedNodeAllocatableEnforcement','HostPortConflict','ContainerFCFailed','Rebooted']
+
 def main():
     try:
         client = get_client(**token_auth)
@@ -28,7 +37,14 @@ def main():
         event_list = v1_events.get()
 
         for event in event_list.items:
-            print(event.metadata.creationTimestamp,"|",event.regarding.kind,"|",event.metadata.name,"|",event.reason,"|",event.note)
+            converted_date = datetime.datetime.strptime(event.metadata.creationTimestamp,"%Y-%m-%dT%H:%M:%SZ")
+            converted_date_utc = pytz.utc.localize(converted_date)
+
+            if check_monitoring_time(converted_date) == True:
+                converted_date_kst = converted_date_utc.astimezone(timezone('Asia/Seoul')).strftime("%Y-%m-%dT%H:%M:%SZ")
+
+                if event.regarding.kind == "Node" and event.reason in nodeEventList:
+                    print(converted_date_kst,"|",event.regarding.kind,"|",event.metadata.name,"|",event.reason,"|",event.note)
             
     except Exception as e:
         traceback.print_exc()
